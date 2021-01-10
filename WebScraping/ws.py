d91abb397ec3f2e09973bfa797fc57558ccbdb9e
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2021-01-09 23:49:36 -0300

    Segurança da Informação com Python

---
+++ WebScraping/ws.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+site = requests.get("https://www.climatempo.com.br/").content
+
+soup = BeautifulSoup(site, 'html.parser')
+
+print(soup.prettify())
+
+temperatura = soup.find("span", class_)
+
